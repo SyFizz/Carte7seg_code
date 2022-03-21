d223e4d8c8d03a4b19133fc092f6096925f508f7
Grégory BERTINI <gbertini.lpo@gmail.com>
2022-03-21 13:57:16 +0100

    Ajout initialisation et brochage

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 
 #include <Arduino.h>
 
+//On définit les branchements
+#define Aff1 2
+#define Aff2 3
+#define Aff3 4
+#define Aff4 5
+
+#define segA 30
+#define segB 31
+#define segC 32
+#define segD 33
+#define segE 34
+#define segF 35
+#define segG 36
+#define dotP 37
+
+#define speaker 8
+
 //Fonction d'initialisation de l'arduino
 void setup() {
-  // put your setup code here, to run once:
+
+
+  for(int i=Aff1; i<=Aff4; i++){
+    pinMode(i, OUTPUT);
+    digitalWrite(i, HIGH);
+  }
+
+  for(int i=segA; i<=dotP; i++){
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+
+  pinMode(speaker, OUTPUT);
+  digitalWrite(speaker, HIGH);
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
